42df2c8fe926225492d7d809c5806c7412be62ae
Lucas <lucasrodriguesdonascimento@outlook.com>
2022-09-25 03:03:24 -0300

    ajuste global using

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -1,7 +1,6 @@
-using GatewayAPI.Application.Extensions.Ocelot;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.AddOcelotConfiguration();
+
 var app = builder.Build();
 app.UseOcelot().Wait();
 app.Run();
